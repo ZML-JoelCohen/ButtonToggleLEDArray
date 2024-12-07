197062333ffaa87095f0eb2278e69f2ddcb39e60
ZML-JoelCohen <joel.cohen@zenmakerlab.com>
2024-12-07 10:52:15 -0800

    Update Button.h

--- Button.h
+++ Button.h
@@ -1,19 +1,25 @@
+#include "HardwareSerial.h"
 #include "Arduino.h"
 #include <stdint.h>
 
 
 class Button {
   public:
-    Button(uint8_t input, uint8_t gnd = 0, uint8_t pwr = 0) {
+    Button() {
+    }
+
+    void init(uint8_t input, uint8_t gnd = 0, uint8_t pwr = 0) {
       input = input;
       gnd = gnd;
       pwr = pwr;
-    }
 
-    void init() {
+      Serial.println("Init called");
       pinMode(input, INPUT);
 
+      Serial.println(String(gnd));
+
       if (gnd) {
+        Serial.println("Starting gnd");
         pinMode(gnd, OUTPUT);
         digitalWrite(gnd, LOW);
       }
